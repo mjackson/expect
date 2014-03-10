1504207dad5532fee1a3bcc3b7984741a1db00a7
Michael Jackson <mjijackson@gmail.com>
2014-03-10 07:34:34 -0700

    Make comparator function optional

--- expect.js
+++ expect.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var util = require('util');
+var inspect = util.inspect;
 
 module.exports = Expectation;
 
@@ -40,30 +41,28 @@ Expectation.prototype.toBeA = toBeA;
 Expectation.prototype.toBeAn = toBeA;
 function toBeA(constructor, message) {
   assert(typeof constructor === 'function', 'The constructor used in toBeA must be a function');
-
-  message = message || util.inspect(this.actual) + ' is not a ' + (constructor.name || constructor.toString());
+  message = message || inspect(this.actual) + ' is not a ' + (constructor.name || constructor.toString());
   assert(this.actual instanceof constructor, message);
 }
 
 Expectation.prototype.toMatch = toMatch;
 function toMatch(pattern, message) {
   assert(util.isRegExp(pattern), 'The pattern used in toMatch must be a RegExp');
-
-  message = message || util.inspect(this.actual) + ' does not match ' + util.inspect(pattern);
+  message = message || inspect(this.actual) + ' does not match ' + inspect(pattern);
   assert(pattern.test(this.actual), message);
 }
 
 Expectation.prototype.toBeLessThan = toBeLessThan;
 Expectation.prototype.toBeFewerThan = toBeLessThan;
 function toBeLessThan(value, message) {
-  message = message || util.inspect(this.actual) + ' is not less than ' + util.inspect(value);
+  message = message || inspect(this.actual) + ' is not less than ' + inspect(value);
   assert(this.actual < value, message);
 }
 
 Expectation.prototype.toBeGreaterThan = toBeGreaterThan;
 Expectation.prototype.toBeMoreThan = toBeGreaterThan;
 function toBeGreaterThan(value, message) {
-  message = message || util.inspect(this.actual) + ' is not greater than ' + util.inspect(value);
+  message = message || inspect(this.actual) + ' is not greater than ' + inspect(value);
   assert(this.actual > value, message);
 }
 
@@ -72,9 +71,13 @@ Expectation.prototype.toContain = toInclude;
 function toInclude(value, comparator, message) {
   assert(Array.isArray(this.actual), 'The actual value used in toContain/toInclude must be an Array');
 
-  message = message || util.inspect(this.actual) + ' does not include ' + util.inspect(value);
-  comparator = comparator || assert.deepEqual;
-  assert(arrayContains(this.actual, comparator, value), message);
+  if (typeof comparator === 'string') {
+    message = comparator;
+    comparator = null;
+  }
+
+  message = message || inspect(this.actual) + ' does not include ' + inspect(value);
+  assert(arrayContains(this.actual, value, comparator), message);
 }
 
 Expectation.prototype.toExclude = toExclude;
@@ -82,20 +85,26 @@ Expectation.prototype.toNotContain = toExclude;
 function toExclude(value, comparator, message) {
   assert(Array.isArray(this.actual), 'The actual value used in toNotContain/toExclude must be an Array');
 
-  message = message || util.inspect(this.actual) + ' includes ' + util.inspect(value);
-  comparator = comparator || assert.deepEqual;
-  assert(!arrayContains(this.actual, comparator, value), message);
+  if (typeof comparator === 'string') {
+    message = comparator;
+    comparator = null;
+  }
+
+  message = message || inspect(this.actual) + ' includes ' + inspect(value);
+  assert(!arrayContains(this.actual, value, comparator), message);
 }
 
 /**
  * Returns true if the given array contains the value, false otherwise. The
  * comparator function must return false or throw to indicate a non-match.
  */
-function arrayContains(array, comparator, value) {
+function arrayContains(array, value, comparator) {
+  comparator = comparator || assert.deepEqual;
+
   return array.some(function (item) {
     try {
       return comparator(item, value) !== false;
-    } catch (e) {
+    } catch (error) {
       return false;
     }
   });
