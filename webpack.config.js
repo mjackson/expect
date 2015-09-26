47642f33824479c224b84ed84365fa523fa9ec05
Michael Jackson <mjijackson@gmail.com>
2015-09-25 17:26:27 -0700

    Inline babel config

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,8 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel?stage=0&loose=all' }
     ]
   }
 
-};
+}
