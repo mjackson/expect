7f79510cd3d49e4931c0dc79b5bef6fdfa1f9253
Michael Jackson <mjijackson@gmail.com>
2015-12-11 13:14:52 -0800

    Update tests

--- modules/__tests__/toInclude-test.js
+++ modules/__tests__/toInclude-test.js
@@ -8,18 +8,26 @@ describe('toInclude', function () {
     }).toThrow(/must be an array or a string/)
   })
 
-  it('does not throw when an array contains the expected value', function () {
+  it('does not throw when an array contains an expected integer', function () {
     expect(function () {
       expect([ 1, 2, 3 ]).toInclude(2)
       expect([ { a: 1 }, { c: 2 } ]).toInclude({ c: 2 })
     }).toNotThrow()
   })
 
-  it('throws when an array does not contain the expected value', function () {
+  it('does not throw when an array contains an expected object', function () {
+    expect(function () {
+      expect([ { a: 1 }, { c: 2 } ]).toInclude({ c: 2 })
+    }).toNotThrow()
+  })
+
+  it('throws when an array does not contain an expected integer', function () {
     expect(function () {
       expect([ 1, 2, 3 ]).toInclude(4)
     }).toThrow(/to include/)
+  })
 
+  it('throws when an array does not contain an expected object', function () {
     expect(function () {
       expect([ { a: 1 }, { c: 2 } ]).toInclude({ a: 2 })
     }).toThrow(/to include/)
