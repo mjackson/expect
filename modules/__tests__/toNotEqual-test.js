3b905c76952994abf79e60b5c1c041bbf9425334
Michael Jackson <mjijackson@gmail.com>
2015-12-16 08:40:23 -0800

    Add failing test
    Illustrates the problem described in #53

--- modules/__tests__/toNotEqual-test.js
+++ modules/__tests__/toNotEqual-test.js
@@ -2,6 +2,36 @@
 import expect from '../index'
 
 describe('toNotEqual', function () {
+  it('works with arrays of objects', function () {
+    const a = [
+      {
+        id: 0,
+        text: 'Array Object 0',
+        boo: false
+      },
+      {
+        id: 1,
+        text: 'Array Object 1',
+        boo: false
+      }
+    ]
+
+    const b = [
+      {
+        id: 0,
+        text: 'Array Object 0',
+        boo: true // value of boo is changed to true here
+      },
+      {
+        id: 1,
+        text: 'Array Object 1',
+        boo: false
+      }
+    ]
+
+    expect(a).toNotEqual(b)
+  })
+
   if (typeof Map !== 'undefined') {
     it('works with Map', function () {
       const a = new Map()
