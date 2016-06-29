578ced27e01ae0a3065591c7f8f3e0559bc320ac
Michael Jackson <mjijackson@gmail.com>
2016-06-29 09:11:48 -0700

    Update build

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,22 @@
 const readFileSync = require('fs').readFileSync
 const execSync = require('child_process').execSync
+const inInstall = require('in-publish').inInstall
 const prettyBytes = require('pretty-bytes')
 const gzipSize = require('gzip-size')
 
-const exec = (command) =>
-  execSync(command, { stdio: 'inherit' })
+if (inInstall())
+  process.exit(0)
 
-exec('npm run build-cjs')
-exec('npm run build-umd')
-exec('npm run build-min')
+const exec = (command, env) =>
+  execSync(command, { stdio: 'inherit', env })
+
+const webpackEnv = Object.assign({}, process.env, {
+  NODE_ENV: 'production'
+})
+
+exec('npm run build-lib')
+exec('npm run build-umd', webpackEnv)
+exec('npm run build-min', webpackEnv)
 
 console.log(
   '\ngzipped, the UMD build is ' + prettyBytes(
