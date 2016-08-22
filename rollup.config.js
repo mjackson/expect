6025525532e6ae672b10a604ba43ac367003722b
Michael Jackson <mjijackson@gmail.com>
2016-08-22 11:07:44 -0600

    Use babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import buble from 'rollup-plugin-buble'
+import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import replace from 'rollup-plugin-replace'
@@ -6,25 +6,27 @@ import replace from 'rollup-plugin-replace'
 const production = process.env.PRODUCTION === 'true';
 
 export default {
-	entry: 'modules/index.js',
-	dest: 'umd/expect.js',
-	format: 'umd',
-	moduleName: 'expect',
-	plugins: [
-		nodeResolve({
-			main: true
-		}),
-		commonjs({
-			include: 'node_modules/**',
-			namedExports: {
-				'define-properties': [ 'supportsDescriptors' ]
-			}
-		}),
-		buble({
-			include: 'modules/**'
-		}),
-		replace({
-			'process.env.NODE_DEBUG': !production
-		})
-	]
+  entry: 'modules/index.js',
+  dest: 'umd/expect.js',
+  moduleName: 'expect',
+  format: 'umd',
+  plugins: [
+    nodeResolve({
+      main: true
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'define-properties': [ 'supportsDescriptors' ]
+      }
+    }),
+    babel({
+      presets: [
+        [ 'es2015', { modules: false } ]
+      ]
+    }),
+    replace({
+      'process.env.NODE_DEBUG': !production
+    })
+  ]
 }
